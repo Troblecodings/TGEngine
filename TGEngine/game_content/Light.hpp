e1bd4325ac0c772614b50e9d1b9ca780f0f933c2
MrTroble <nifroh56@gmail.com>
2019-02-08 22:34:50 +0100

    Added docs

--- TGEngine/game_content/Light.hpp
+++ TGEngine/game_content/Light.hpp
@@ -3,8 +3,17 @@
 #include "../Stdbase.hpp"
 #include "../pipeline/buffer/UniformBuffer.hpp"
 
-extern UniformBuffer light_buffer;
+extern UniformBuffer light_buffer; // the light buffer for the postion of the light
 
+/* 
+ * Internal method to initialize the light buffer
+ */
+INTERNAL
+SINCE(0, 0, 4)
 void initLight();
 
+/*
+ * Change the possition of the diffuse light
+ */
+SINCE(0, 0, 4)
 void setLightPosition(glm::vec3 pos);
