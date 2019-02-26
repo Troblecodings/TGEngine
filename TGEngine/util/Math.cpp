0de21916cdaaa489324d896c2625479d363a7ee4
Pixly <thepixly@pm.me>
2019-02-26 19:13:34 +0100

    Added gaussian function and accurate color mixing to math.hpp/math.cpp

--- TGEngine/util/Math.cpp
+++ TGEngine/util/Math.cpp
@@ -2,6 +2,21 @@
 
 namespace math {
 
+	glm::vec4 mixColor(glm::vec4 a, glm::vec4 b, float t = 0.5f)
+	{
+		float it = 1 - t; // Inverse time
+		return glm::vec4(
+			sqrtf32(a.x * a.x * t + b.x * b.x * it),
+			sqrtf32(a.y * a.y * t + b.y * b.y * it),
+			sqrtf32(a.z * a.z * t + b.z * b.z * it),
+			sqrtf32(a.w * a.w * t + b.w * b.w * it)
+		);
+	}
+
+	float calculateNormalDistribution(float x, float mu = 0.0f, float sigma = 1.0f) {
+		return (1 / (sigma * SQRT_PIx2)) * powf32(EULER, powf32(x - mu, 2) / (2 * sigma * sigma));
+	}
+
 	float f_max(float val1, float val2) {
 		if (val1 > val2) {
 			return val1;
--- TGEngine/util/Math.hpp
+++ TGEngine/util/Math.hpp
@@ -2,11 +2,21 @@
 
 #include "Annotations.hpp"
 #include <cstdint>
+#include <cmath>
+#include "glm/glm.hpp"
 
 #define PI 3.14159265358979323846
 #define PIx2 6.28318530717958647692
+#define SQRT_PIx2 2.506628274631
+#define EULER 2.71828182845904523536
 
 namespace math {
+
+	SINCE(0, 0, 4)
+	glm::vec4 mixColor(glm::vec4 a, glm::vec4 b, float t = 0.5f);
+
+	SINCE(0, 0, 4)
+	float calculateNormalDistribution(float x, float mu = 0.0f, float sigma = 1.0f);
 
 	SINCE(0, 0, 1)
 	float f_max(float val1, float val2);
