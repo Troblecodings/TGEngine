56850d6dbc085a8c554cc4d32405b07ef3107c02
Pixly <thepixly@pm.me>
2019-02-26 20:00:45 +0100

    Added Beziercurve I hope it works lol

--- TGEngine/drawlib/Beziercurve.cpp
+++ TGEngine/drawlib/Beziercurve.cpp
@@ -1,5 +1,37 @@
 #include "Beziercurve.hpp"
 
-void drawBeziercurve(TGVertex ver1, TGVertex ver2, TGVertex ver3, int max_verticies, VertexBuffer* verticeis) {
-	
+static TGVertex midpoint(TGVertex a, TGVertex b, float t)
+{
+    return {
+        b.position + (a.position - b.position) * t,
+        mixColor(a.color, b.color, t),
+        b.uv + (a.ub - b.uv) * t,
+        COLOR_ONLY
+    };
+}
+
+void drawBeziercurve(VertexBuffer* vertices, IndexBuffer* indices, size_t max_vertices, vector<TGVertex>* points) {
+    if(points->size() == 0) return;
+    if(points->size() == 1)
+    {
+        vertices->add(points->at(0));
+        return;
+    }
+    if(points->size() == 2)
+    {
+        for(float i = 0; i < max_vertices; i++)
+            vertices->add(midpoint(points->at(0), points->at(1), i / (max_vertices - 1));
+        return;
+    }
+    for(float t = 0; t < max_vertices; t++)
+    {
+        TGVertex buffer[points->size() - 1];
+        for(int i = 0; i < max_vertices - 1; i++)
+            buffer[i] = midpoint(points->at(i), points->at(i + 1), t / (max_vertices - 1));
+        for(int i = max_vertices - 1; i > 2; i--)
+            for(int j = 1; j < i - 1; j++)
+                buffer[j - 1] = midpoint(buffer[j - 1], buffer[j], t / (max_vertices - 1));
+        vertices->add(buffer[0]);
+        indices->addIndex(vertices->count_of_points - 1);
+    }
 }
--- TGEngine/drawlib/Beziercurve.hpp
+++ TGEngine/drawlib/Beziercurve.hpp
@@ -1,3 +1,6 @@
 #pragma once
 
 #include "BaseDrawLib.hpp"
+
+SINCE(0, 0, 4)
+void drawBeziercurve(VertexBuffer* vertices, IndexBuffer* indices, size_t max_vertices, vector<TGVertex>* points);
