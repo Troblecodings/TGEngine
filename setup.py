9abb6818c9d03eb476bdd6b144ae5c8e1d693285
MrTroble <nifroh56@gmail.com>
2019-03-07 20:06:14 +0100

    Removed debugging

--- setup.py
+++ setup.py
@@ -137,8 +137,6 @@ def callback(cob, size, total):
 
 
 def getstb():
-    if os.path.exists("stb"):
-        print(len(os.listdir("stb")))
     if os.path.exists("stb") and len(os.listdir("stb")) > 0:
         print("Updating stb")
         p = subprocess.Popen(["git", "pull"], cwd="stb")
