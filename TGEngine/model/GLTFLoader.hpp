30e80ed7a33a9e8c5d582ab06460f41ef3d9da2a
MrTroble <nifroh56@gmail.com>
2019-05-03 14:07:42 +0200

    Docs change

---
+++ TGEngine/model/GLTFLoader.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "tiny_gltf.h"
+#include "../io/Files.hpp"
+
+namespace tg_model {
+
+	SINCE(0, 0, 4)
+	void loadGLTFModel(char* name);
+
+}
