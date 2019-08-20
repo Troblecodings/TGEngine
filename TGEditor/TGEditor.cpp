640916b07314377a03e10d9c9b91c36bd80486fd
MrTroble <nifroh56@gmail.com>
2019-08-20 15:25:25 +0200

    Fixed build order

--- TGEditor/TGEditor.cpp
+++ TGEditor/TGEditor.cpp
@@ -15,12 +15,12 @@ int main(int argc, char** args) {
 	initEngine(&editor.main_window);
 	tge::gmc::Actor actor2;
 
-	tge::mdl::loadGltf(argc > 1 ? args[1] : "resource\\glTF-Sample-Models\\2.0\\VC\\glTF\\VC.gltf", &actor2);
+	tge::mdl::loadGltf(argc > 1 ? args[1] : "resource\\glTF-Sample-Models\\2.0\\Cube\\glTF\\Cube.gltf", &actor2);
 	tge::gmc::actors.push_back(&actor2);
 
 	std::cout << "Starting Editor" << std::endl;
 
-	tge::gmc::LightActor light = tge::gmc::LightActor(glm::vec3(0.05, 0.05, 0.05), glm::vec3(0, 0, 0));
+	tge::gmc::LightActor light = tge::gmc::LightActor(2, glm::vec3(0.05, 0.05, 0.05), glm::vec3(0, 0, 0));
 	tge::gmc::lights.push_back(&light);
 
 	startTGEngine(&editor.main_window);
