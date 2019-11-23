9cc2ec07b13bc3c10a4cc3b32fe415eba9361871
MrTroble <nifroh56@gmail.com>
2019-11-23 23:06:41 +0100

    First commtits for rewrite of actor system

--- TGEngine/gamecontent/Actor.hpp
+++ TGEngine/gamecontent/Actor.hpp
@@ -8,49 +8,25 @@
 #include "../util/VectorUtil.hpp"
 #include "Material.hpp"
 
-namespace tge {
-	namespace gmc {
+namespace tge::gmc {
 
-		struct AABB {
-			glm::vec3 min;
-			glm::vec3 max;
+	struct Actor {
+		Actor* parent;
+		Mesh* mesh;
 
-			void print();
-		};
+		std::vector<TGVertex> vertices;
+		std::vector<uint32_t> indices;
 
-		struct Actor {
+		glm::mat4 matrix = glm::mat4(1);
+		glm::quat rotation = glm::quat(0, 0, 0, 0);
+		glm::vec3 translation = glm::vec3(0);
+		glm::vec3 scale = glm::vec3(1);
+	};
 
-			Actor* parent;
-			Mesh* mesh;
-			AABB aabb;
+	struct Model {
+		std::vector<Actor*> actors;
+		std::vector<Material> materials;
+	};
 
-			glm::mat4 matrix = glm::mat4(1);
-			glm::quat rotation = glm::quat(0, 0, 0, 0);
-			glm::vec3 translation = glm::vec3(0);
-			glm::vec3 scale = glm::vec3(1);
-
-			/*
-             * Returns the overall transform matrix from this Actor
-             */
-			SINCE(0, 0, 4)
-			glm::mat4 transforms();
-
-
-			/*
-			 * Returns the overall transform matrix from this Actor
-			 */
-			SINCE(0, 0, 4)
-			glm::mat4 localTransform();
-		};
-
-		struct Model {
-			AABB aabb;
-
-			std::vector<Actor*> actors;
-			std::vector<VkSampler> samplers;
-			std::vector<Material> materials;
-		};
-
-		extern std::vector<Model*> models;
-	}
+	extern std::vector<Model*> models;
 }
