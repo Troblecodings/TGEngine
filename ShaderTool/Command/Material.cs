cfa7d2cdb0a428a050d8f659391e458107018b3d
IceDynamix <aerozocker@gmail.com>
2019-11-29 20:12:47 +0100

    Create basic Material class

---
+++ ShaderTool/Command/Material.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ShaderTool.Error;
+using static ShaderTool.Util.Util;
+
+namespace ShaderTool.Command {
+    class Material {
+
+        public static int MaterialCommand(string[] args) {
+            AsssertNoneNull(args);
+
+            switch (args[0]) {
+                case "add":
+                    return MaterialAdd(GetParams(args));
+                case "rm":
+                    return 0;
+                case "make":
+                    return 0;
+                case "list":
+                    return 0;
+                case "show":
+                    return 0;
+            }
+
+            Console.WriteLine("Wrong parameters! Must be add/rm/make/list/show!");
+            return WRONG_PARAMS;
+        }
+
+        public static int MaterialAdd(string[] args) {
+            AsssertNoneNull(args);
+            return 0;
+        }
+
+    }
+
+    class MaterialData {
+        float[] color;
+        uint diffuseTexture;
+    }
+}
