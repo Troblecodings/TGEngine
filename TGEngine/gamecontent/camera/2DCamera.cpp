6e8532620434b5172457881172a793ebb1f6fe0d
MrTroble <nifroh56@gmail.com>
2020-01-13 15:07:22 +0100

    Fixed draw ordering

--- TGEngine/gamecontent/camera/2DCamera.cpp
+++ TGEngine/gamecontent/camera/2DCamera.cpp
@@ -7,7 +7,7 @@ void tge::gmc::setTopDownCamera(TopDownCamera* camera) {
 	glm::mat4 mat =
 	{ camera->screenheight / (float)camera->screenwidth, 0, 0, camera->positionx,
 		0, 1, 0, camera->positiony,
-		0, 0, -0.1, 0,
+		0, 0, 0.2, 0,
 		0, 0, 0, 1
 	};
 	fillUniformBuffer(TRANSFORM_BUFFER, &mat, sizeof(glm::mat4));
