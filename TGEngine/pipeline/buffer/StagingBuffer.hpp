ebf06edb7df496493efc2c82f518dee73122e26b
MrTroble <nifroh56@gmail.com>
2020-01-14 20:57:42 +0100

    Update StagingBuffer.hpp

--- TGEngine/pipeline/buffer/StagingBuffer.hpp
+++ TGEngine/pipeline/buffer/StagingBuffer.hpp
@@ -6,7 +6,10 @@
 #include "Memory.hpp"
 
 /*
- * 
+ * Holds information to create a staging buffer
+ * A stanging buffer is a transfer buffer
+ * It transferse data from a host visible memory
+ * onto device local memory
  */
 struct StagingBuffer {
 	VkDeviceSize   size;
