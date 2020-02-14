bc4ab721dcb4f864ff13cc4fbfcadc66e6c6a258
MrTroble <nifroh56@gmail.com>
2020-02-14 23:57:01 +0100

    Fixed stuff

--- TGEngine/io/Resource.cpp
+++ TGEngine/io/Resource.cpp
@@ -29,16 +29,18 @@ Map loadResourceFile(char* name) {
 	std::vector<TextureInputInfo> textureInfos;
 	textureInfos.reserve(2048);
 
+	fread(&blocklength, sizeof(uint32_t), 1, file);
+
 	while (blocklength != UINT32_MAX)
 	{
-		fread(&blocklength, sizeof(uint32_t), 1, file);
-
 		stbi_uc* resbuffer = new stbi_uc[blocklength];
 		fread(resbuffer, sizeof(char), blocklength, file);
 
 		TextureInputInfo inputInfo;
 		inputInfo.data = stbi_load_from_memory(resbuffer, (int)blocklength, &inputInfo.x, &inputInfo.y, &inputInfo.comp, STBI_rgb_alpha);
 		textureInfos.push_back(inputInfo);
+
+		fread(&blocklength, sizeof(uint32_t), 1, file);
 	}
 
 	TextureBindingInfo bindingInfo[2048];
@@ -53,38 +55,55 @@ Map loadResourceFile(char* name) {
 	std::vector<Material> materials;
 	materials.reserve(2048);
 
+	fread(&blocklength, sizeof(uint32_t), 1, file);
+
 	while (blocklength != UINT32_MAX)
 	{
-		fread(&blocklength, sizeof(uint32_t), 1, file);
-
 		Material material;
 		fread(&material, sizeof(Material), 1, file);
 
 		materials.push_back(material);
+
+		fread(&blocklength, sizeof(uint32_t), 1, file);
 	}
 
 	if (feof(file) != 0)
 		return map;
 
+	// Start to read the actor
 	std::vector<ActorInputInfo> actors;
 	actors.reserve(2048);
 
+	// Read the block size of the following content
+	fread(&blocklength, sizeof(uint32_t), 1, file);
+
+	// Goes on until it hits a change request block size (2^32)
 	while (blocklength != UINT32_MAX)
 	{
-		fread(&blocklength, sizeof(uint32_t), 1, file);
-
+		// Reads the actor properties and so on
 		ActorInputInfo actorInfo;
 		fread(&actorInfo, sizeof(ActorInputInfo), 1, file);
 
+		// Reads the indicies from the file
 		actorInfo.pIndices = new uint32_t[actorInfo.indexCount]; // Object lifetime ?
 		fread(&actorInfo.pIndices, sizeof(uint32_t), actorInfo.indexCount, file);
 
+		// calculates the length of the verteicies to read
 		uint32_t len = blocklength - (sizeof(ActorInputInfo) + sizeof(uint32_t) * actorInfo.indexCount);
 
+		// Reads the verticies
 		actorInfo.pVertices = new uint8_t[len]; // Object lifetime?
 		fread(actorInfo.pVertices, sizeof(uint8_t), len, file);
 
+		// Not sure about object lifetime here ... problem could be that the objects are being deleted
+		// if the method exits. However those should be part of the ouput
+		// which is returned ... this hover is only a local variable
+		// Needs research
+
 		actors.push_back(actorInfo);
+
+		// Read the next block size
+		fread(&blocklength, sizeof(uint32_t), 1, file);
 	}
 
 	createActor(actors.data(), (uint32_t)actors.size());
