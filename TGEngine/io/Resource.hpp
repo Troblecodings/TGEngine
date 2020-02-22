1d63f5c31a742b2d0177b02190f88db4f8780fe3
MrTroble <nifroh56@gmail.com>
2020-02-22 01:13:27 +0100

    Minor improvements

--- TGEngine/io/Resource.hpp
+++ TGEngine/io/Resource.hpp
@@ -2,14 +2,15 @@
 
 #include "../Stdbase.hpp"
 #include "../gamecontent/Material.hpp"
+#include "../gamecontent/Actor.hpp"
 #include "../pipeline/buffer/Texturebuffer.hpp"
 
 #define TGR_VERSION_1 1
 
 struct Map {
-	std::vector<TextureInputInfo> textures;
-	std::vector<Material> materials;
-	std::vector<ActorInputInfo> actors;
+	std::vector<tge::tex::TextureInputInfo> textures;
+	std::vector<tge::gmc::Material> materials;
+	std::vector<tge::gmc::ActorInputInfo> actors;
 };
 
 void loadResourceFile(const char* name, Map* map);
