ec1fcfd843a0758cecaae435aefc775bb75b56ab
Clazilla <47395364+Clazilla@users.noreply.github.com>
2020-02-27 00:14:34 +0100

    added Fonfile read

--- TGEngine/io/Font.cpp
+++ TGEngine/io/Font.cpp
@@ -1,53 +1,20 @@
 #include "Font.hpp"
 #include "../gamecontent/camera/Camera.hpp"
+#include "Files.hpp"
+#include "../pipeline/buffer/Texturebuffer.hpp"
 
 namespace tge {
 	namespace fnt {
-
-		Font::Font(char* path, uint32_t height) : height(height) {
-			uint8_t* fileData = tge::nio::readAll(path);
-			ASSERT_NONE_NULL_DB(fileData, "File data nullptr!", TG_ERR_DB_NULLPTR)
-
-				uint8_t* tempbitmap = new uint8_t[(size_t)height * (size_t)4096 * (size_t)height];
-			stbtt_BakeFontBitmap(fileData, 0, (float)height, tempbitmap, height * 64, height * 64, 0, 256, this->cdata);
-
-			delete[] fileData;
-
-			uint8_t* colorData = new uint8_t[(size_t)height * (size_t)16384 * (size_t)height];
-
-			for (size_t i = 0; i < ((size_t)height * (size_t)4096 * (size_t)height); i++) {
-				colorData[i * 4] = tempbitmap[i];
-				colorData[i * 4 + 1] = tempbitmap[i];
-				colorData[i * 4 + 2] = tempbitmap[i];
-				colorData[i * 4 + 3] = tempbitmap[i];
-			}
-			delete[] tempbitmap;
-
-			//this->texture = new tex::Texture(height * 64, height * 64);
-		}
-
-		void Font::drawString(glm::vec2 pos, char* text, VertexBuffer* buffer, IndexBuffer* ibuffer, glm::vec4 color) {
-			pos.y /= 0.002f;
-			pos.x /= gmc::multiplier * 0.002f;
-			while (*text) {
-				text++;
-			}
-		}
-
-		uint32_t Font::getMaterialIndex() {
-			return this->materialIndex;
-		}
-
-		glm::vec2 Font::getExtent(char* chr) {
-			glm::vec2 pos = glm::vec2(0, 0);
-			while (*chr) {
-				//stbtt_aligned_quad quad;
-				//stbtt_GetBakedQuad(this->cdata, this->texture->getWidth(), this->texture->getHeight(), *chr, &pos.x, &pos.y, &quad, 0);
-				chr++;
-			}
-			pos.y = this->height * 0.0015f;
-			pos.x *= gmc::multiplier * 0.002f;
-			return pos;
+		void readFontfile(const char* name, const float fontheight, tge::tex::TextureInputInfo* textureInputInfo) {
+			long size = 0;
+			textureInputInfo->data = new uint8_t[16000000];
+			stbtt_bakedchar charData[256];
+			textureInputInfo->x = 2000;
+			textureInputInfo->y = 2000;
+			textureInputInfo->comp = 4;
+			uint8_t* filedata = tge::nio::readAll(name, &size);
+			stbtt_BakeFontBitmap(filedata, 0, fontheight, textureInputInfo->data, 2000, 2000, 0, 256, charData);
+			
 		}
 	}
 }
--- TGEngine/io/Font.hpp
+++ TGEngine/io/Font.hpp
@@ -13,32 +13,5 @@
 namespace tge {
 	namespace fnt {
 
-		/*
-		 * <strong>This API is deprecated!</strong>
-		 */
-		class Font {
-
-		private:
-			uint32_t height = 20;
-			stbtt_bakedchar cdata[256];
-			uint32_t materialIndex = -1;
-
-		public:
-			/*
-			* Initializes all values (besides default constructor)
-			* Adds new font to the cache and adds the generated texture +  it's material (UI)
-			*/
-			Font(char* path) : Font(path, 30) {}
-			Font(char* path, uint32_t height);
-
-			/*
-			* Draws a string to the screen (UI only)
-			*/
-			void drawString(glm::vec2 pos, char* string, VertexBuffer* buffer, IndexBuffer* ibuffer, glm::vec4 color);
-
-			uint32_t getMaterialIndex();
-
-			glm::vec2 getExtent(char* chr);
-		};
 	}
 }
