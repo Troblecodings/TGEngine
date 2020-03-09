14579ad5ef9853578510bef2b280832d2b20b2ab
MrTroble <nifroh56@gmail.com>
2020-03-09 09:13:55 +0100

    Removed unused

--- TGEngine/TGEngine.hpp
+++ TGEngine/TGEngine.hpp
@@ -4,7 +4,6 @@
 #include "pipeline/Pipeline.hpp"
 #include "pipeline/Draw.hpp"
 #include "gamecontent/Actor.hpp"
-#include "ui/UIEntity.hpp"
 #include "gamecontent/camera/Camera.hpp"
 
 extern VertexBuffer vertexBuffer;
