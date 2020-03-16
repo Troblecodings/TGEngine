5a7f41b368312f8e70bb27070a07d127de57edda
MrTroble <nifroh56@gmail.com>
2020-03-16 13:48:02 +0100

    Added typedefs

--- TGEngine/pipeline/Pipe.hpp
+++ TGEngine/pipeline/Pipe.hpp
@@ -5,14 +5,17 @@
 
 namespace tge::pip {
 
+	typedef VkCullModeFlags CullMode;
+	typedef VkPolygonMode PolygonMode;
+
 	struct PipelineInputInfo {
-		ShaderPipe      pipe;
-		uint32_t        stride;
-		uint32_t        width;
-		uint32_t        height;
-		VkCullModeFlags cullMode;
-		VkPolygonMode   polygonMode;
-		float           lineWidth;
+		ShaderPipe  pipe;
+		uint32_t    stride;
+		uint32_t    width;
+		uint32_t    height;
+		CullMode    cullMode;
+		PolygonMode polygonMode;
+		float       lineWidth;
 	};
 
 	extern VkPipeline defaultPipeline;
