9fffd270d7395fb6e0985d24b9a9e5e2b1a9b67a
MrTroble <nifroh56@gmail.com>
2020-07-18 20:24:18 +0200

    Added more tests

--- ShaderTool/Test/tests.py
+++ ShaderTool/Test/tests.py
@@ -20,9 +20,9 @@ def runcommand(command, console, toolpath):
         if "Exit code " in outstr:
             result[0] = int(re.split(r"(\\r|\\n)", outstr.split("Exit code ")[1])[0].strip())
     if console & 2 == 2:
+        rtc = subprocess.call(toolpath + " " + command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         # This is bullshit
         # Who the fuck thought that negative numbers could come out of this
-        rtc = subprocess.call(toolpath + " " + command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         if rtc == 0:
             result[1] = 0
         else:
@@ -46,22 +46,48 @@ TESTS = {
     ("map add " + rng(r"[\w\s]"), 3, (-2, -2)),
     ("map add", 3, (-1, -1)),
     ("map", 3, (-1, -1)),
+
     ("setcwd", 3, (-1, -1)),
     ("settool", 3, (0, 0)),
     ("setcwd " + pt.abspath(os.getcwd()), 3, (0, 0)),
+
+    ("pipe", 3, (-1, -1)),
+    ("pipe create", 3, (-1, -1)),
+    ("pipe delete", 3, (-1, -1)),
+    ("pipe show", 3, (-1, -1)),
+    ("pipe update", 3, (-1, -1)),
+    ("pipe list", 3, (0, 0)),
+    ("pipe make", 3, (0, 0)),
+
+    ("shader", 3, (-1, -1)),
+    ("texture", 3, (-1, -1)),
+    ("actor", 3, (-1, -1)),
+    ("map", 3, (-1, -1)),
+    ("material", 3, (-1, -1)),
+    ("font", 3, (-1, -1)),
 }
+
+FAILED = False
 
 def runtest(toolpath):
     shutil.rmtree("Resources")
+    for x in os.listdir("."):
+        if x == "tests.py":
+            continue
+        os.remove(x)
     for test in TESTS:
         output = runcommand(test[0], test[1], toolpath)
         if output == test[2]:
             print("Test passed!")
         else:
             print("Failed at " + str(test) + " Output: " + str(output))
+            FAILED = True
 
 
 for path, name, files in os.walk("../bin"):
     if "ShaderTool.exe" in files:
         toolpath = pt.abspath(pt.join(path, "ShaderTool.exe")).replace("\\", "/")
         runtest(toolpath)
+
+if FAILED:
+    exit(1)
